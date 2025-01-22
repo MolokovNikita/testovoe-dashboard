b0af45a2adcd8d5bf2cf653cfc7608385112a231
DekuFoo <ggmaster039@gmail.com>
2025-01-22 15:30:51 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { sequelize } = require('./models');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+app.use(bodyParser.json());
+
+// const authorRoutes = require('./routes/authors');
+// const bookRoutes = require('./routes/books');
+
+// app.use('/api/authors', authorRoutes);
+// app.use('/api/books', bookRoutes);
+
+const PORT = process.env.PORT || 5002;
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: CLIENT_URL,
+    allowedHeaders: ["Authorization", "Content-Type", "x-token"],
+  }),
+);
+
+app.listen(PORT, async () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+//   await sequelize.authenticate();
+  console.log('Подключение к базе данных успешно!');
+});
